73dde30bba0bdf8dee12608ba8eccb23ede5612a
SwpRoman <crypto.labs.node@gmail.com>
2025-03-12 14:03:54 +0300

    solve counter task

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,25 +1,47 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const MIN_VALUE = 0;
+    const MAX_VALUE = 5;
+
+    const counter = ref(0);
+
+    function incrementValue() {
+
+      if (counter.value < MAX_VALUE) counter.value++
+    }
+    function decrementValue() {
+      if (counter.value > MIN_VALUE) counter.value--
+    }
+
+    return {
+      counter,
+      incrementValue,
+      decrementValue,
+      MAX_VALUE,
+      MIN_VALUE
+    }
+  },
 
   template: `
     <div class="counter">
-      <button
+      <button @click="decrementValue"
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="counter == MIN_VALUE"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ counter }}</span>
 
-      <button
+      <button @click="incrementValue"
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="counter == MAX_VALUE"
       >➕</button>
     </div>
   `,
