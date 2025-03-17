dd8b4388d9ff0e3efbee0fa77294d34fdc273c4a
SwpRoman <crypto.labs.node@gmail.com>
2025-03-17 13:58:38 +0300

    solve removable emails task

--- 03-components/30-removable-emails/EmailListItem.js
+++ 03-components/30-removable-emails/EmailListItem.js
@@ -2,23 +2,23 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'EmailListItem',
-
+  emits: ['delete'],
   props: {
     email: {
       type: String,
       required: true,
     },
-
     marked: {
       type: Boolean,
       default: false,
     },
   },
+  
 
   template: `
     <li :class="{ marked }">
       {{ email }}
-      <button type="button" aria-label="Удалить" @click.stop>❌</button>
+      <button type="button" aria-label="Удалить" @click="$emit('delete')">❌</button>
     </li>
   `,
 })
