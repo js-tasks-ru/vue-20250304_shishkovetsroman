31a9eab2cd13eaacb532221743ababed30a8d3eb
SwpRoman <crypto.labs.node@gmail.com>
2025-03-17 13:29:32 +0300

    solve uiClock task

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,32 @@
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeUnmount, onMounted, ref } from 'vue'
+
+const getCurrentTime = () => {
+
+  const timeFormatter = Intl.DateTimeFormat(navigator.language, {
+    timeStyle: "medium",
+  })
+  return timeFormatter.format(Date.now())
+}
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    let intervalId;
+    const currentTime = ref(getCurrentTime());
 
-  template: `<div class="clock">10:12:02</div>`,
+    onMounted(() => {
+      intervalId = setInterval(() => currentTime.value = getCurrentTime(), 1000)
+    })
+
+    onBeforeUnmount(() => {
+      clearInterval(intervalId)
+    })
+
+    return {
+      currentTime
+    }
+  },
+
+  template: `<div class="clock">{{ currentTime }}</div>`,
 })
